03ac7c692be55780ec1fb2942216ad5afa97dd26
hady <hady207@hotmail.com>
2022-04-10 20:15:43 +0300

    deleted analytic module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,16 @@ import { UserModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { BookingsModule } from './bookings/bookings.module';
-import { AnalyticsModule } from './analytics/analytics.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.DB_URL, {
       autoIndex: true,
     }),
-    StoreModule,
-    UserModule,
     AuthModule,
+    UserModule,
+    StoreModule,
     BookingsModule,
-    AnalyticsModule,
   ],
 })
 export class AppModule {}
